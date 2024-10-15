88e3e6d24f80e88289e7de527cc767e5b5bc51fc
johnfernandis1999 <140940560+johnfernandis1999@users.noreply.github.com>
2024-10-15 22:06:58 +0530

    file commited

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="p-3 text-bg-dark">
+    <header className="p-3 text-bg-dark post-header">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <a
